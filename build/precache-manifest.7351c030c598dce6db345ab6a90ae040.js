99afa8c5fe5d1b9e0c72ec40503bf7adf683913d
midnight-sl <sofilogvin@gmail.com>
2020-01-20 18:00:01 +0200

    GitPages build info added

---
+++ build/precache-manifest.7351c030c598dce6db345ab6a90ae040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea261c52ea9ba8843e856bc90474a8f0",
+    "url": "/radio-app/index.html"
+  },
+  {
+    "revision": "1d49537b8c40fc35a1a7",
+    "url": "/radio-app/static/css/main.47891f1a.chunk.css"
+  },
+  {
+    "revision": "ddf8725bba055deb3a0d",
+    "url": "/radio-app/static/js/2.e41681c1.chunk.js"
+  },
+  {
+    "revision": "0e26f4a19a3697556c52a337046756fe",
+    "url": "/radio-app/static/js/2.e41681c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d49537b8c40fc35a1a7",
+    "url": "/radio-app/static/js/main.6dfcdc54.chunk.js"
+  },
+  {
+    "revision": "63ec8977eaebd38029c9",
+    "url": "/radio-app/static/js/runtime-main.bcc8ef95.js"
+  }
+]);
